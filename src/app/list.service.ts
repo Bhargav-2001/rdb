265eb9f09cc6cd323bbf76d45383468e4621d730
bhargav <bhargavvvkumar2001@gmail.com>
2023-07-27 11:07:02 +0530

    last

--- src/app/list.service.ts
+++ src/app/list.service.ts
@@ -1,3 +1,5 @@
+// list.service.ts
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -24,8 +26,9 @@ export interface ApiResponse {
 export class ListService {
   private apiUrl = 'http://nhsappchna6210.cscidp.net/rdb/api/employee';
   private readonly pivotValue = 'Release';
-  private readonly pivotValue1='Designation';
-  // private readonly pivotValue2='resign;'
+  private readonly pivotValue1 = 'Designation';
+  private readonly pivotValue2 = 'Resigned';
+  private readonly pivotValue3 = 'ONBOARDING';
 
   constructor(private http: HttpClient) { }
 
@@ -33,8 +36,19 @@ export class ListService {
     const url = `${this.apiUrl}?pivot=${this.pivotValue}`;
     return this.http.get<ApiResponse[]>(url);
   }
-  fetchData1(): Observable<ApiResponse[]> {
+
+  fetchDataForDesignation(): Observable<ApiResponse[]> {
     const url = `${this.apiUrl}?pivot=${this.pivotValue1}`;
     return this.http.get<ApiResponse[]>(url);
+  }
+
+  fetchDataForResigned(): Observable<ApiResponse[]> {
+    const url = `${this.apiUrl}?pivot=${this.pivotValue2}`;
+    return this.http.get<ApiResponse[]>(url);
+  }
+
+  fetchDataForOnboarding(): Observable<any> {
+    const url = `${this.apiUrl}?pivot=${this.pivotValue3}`;
+    return this.http.get<any>(url);
   }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -8,7 +8,9 @@ import { ListService, ApiResponse } from '../list.service';
 })
 export class ListComponent implements OnInit {
   apiResponse: ApiResponse[] = [];
-  apiResponse1: ApiResponse[] = [];
+  apiResponseDesignation: ApiResponse[] = [];
+  apiResponseResigned: ApiResponse[] = [];
+  apiResponseOnboarding: ApiResponse[] = [];
   tableHeadings: string[] = [
     'Value',
     'Count',
@@ -19,31 +21,59 @@ export class ListComponent implements OnInit {
 
   ngOnInit() {
     // Fetch data for 'Release'
-    this.fetchData();
+    this.fetchDataForRelease();
     // Fetch data for 'Designation'
-    this.fetchData1();
+    this.fetchDataForDesignation();
+    // Fetch data for 'Resigned'
+    this.fetchDataForResigned();
+    // Fetch data for 'ONBOARDING'
+    this.fetchDataForOnboarding();
   }
 
-  fetchData() {
+  fetchDataForRelease() {
     this.listService.fetchData().subscribe(
       (response) => {
         // Concatenate the response to apiResponse array
         this.apiResponse = this.apiResponse.concat(response);
       },
       (error) => {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data for Release:', error);
       }
     );
   }
 
-  fetchData1() {
-    this.listService.fetchData1().subscribe(
+  fetchDataForDesignation() {
+    this.listService.fetchDataForDesignation().subscribe(
       (response) => {
-        // Concatenate the response to apiResponse array
-        this.apiResponse1 = this.apiResponse1.concat(response);
+        // Concatenate the response to apiResponseDesignation array
+        this.apiResponseDesignation = this.apiResponseDesignation.concat(response);
       },
       (error) => {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data for Designation:', error);
+      }
+    );
+  }
+
+  fetchDataForResigned() {
+    this.listService.fetchDataForResigned().subscribe(
+      (response) => {
+        // Concatenate the response to apiResponseResigned array
+        this.apiResponseResigned = this.apiResponseResigned.concat(response);
+      },
+      (error) => {
+        console.error('Error fetching data for Resigned:', error);
+      }
+    );
+  }
+
+  fetchDataForOnboarding() {
+    this.listService.fetchDataForOnboarding().subscribe(
+      (response:any) => {
+        // Concatenate the response to apiResponseOnboarding array
+        this.apiResponseOnboarding = this.apiResponseOnboarding.concat(response);
+      },
+      (error) => {
+        console.error('Error fetching data for ONBOARDING:', error);
       }
     );
   }
